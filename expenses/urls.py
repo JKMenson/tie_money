3657408293408eadb3640cf34f30aedea5a2052b
Jonah K Menson <jonahmensonit@gmail.com>
2024-05-26 20:28:32 +0200

    first commit

---
+++ expenses/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.index, name="expenses"),
+    path('add_expenses', views.add_expenses, name="add_expenses")
+]
